42702dd1448a3a8e7c023a94e378517ea7a319e2
abhinsudha <abhinsudha@gmail.com>
2022-03-08 12:35:04 +0530

    change index file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <StrictMode.ReactDOM>
+  <>
+
     <App />
-  </StrictMode.ReactDOM>,
+  </>,
   document.getElementById('root')
 );
 
