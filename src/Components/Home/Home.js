3b889c8b00c3efbf3894759176e0c9752e52b0f0
abhinsudha <abhinsudha@gmail.com>
2022-04-23 18:33:36 +0530

    remove popup open button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -71,7 +71,7 @@ function Home() {
 
 
             <div>
-                <Button onClick={handleOpen}>Open modal</Button>
+                {/* <Button onClick={handleOpen}>Open modal</Button> */}
                 <Modal
                     open={open}
                     onClose={handleClose}
